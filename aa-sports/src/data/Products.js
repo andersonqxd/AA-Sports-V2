f41c0749b73a0c5d8e75ebcdb73daaceddc655dd
Gabriel <gabrielfelipedev85@gmail.com>
2025-07-16 23:14:34 -0300

    Inserindo Imagens dos produtos

--- aa-sports/src/data/Products.js
+++ aa-sports/src/data/Products.js
@@ -113,5 +113,253 @@ export const products = [
     name: "Bicicleta Aro 29 KRW",
     price: 1899.00,
     image: "20.webp"
-  }
+  },
+  {
+    id: 21,
+    name: "Tênis Nike Court Vision Low Next",
+    price: 379.99,
+    image: "21.webp"
+  },
+  {
+    id: 22,
+    name: "Tênis Nike Air Max Impact 4 Masculino",
+    price: 370.49,
+    image: "22.webp"
+  },
+  {
+    id: 23,
+    name: "Tênis Nike Air Max Excee Masculino",
+    price: 446.49,
+    image: "23.webp"
+  },
+  {
+    id: 24,
+    name: "Tênis Nike Downshifter 13 Feminino",
+    price: 408.49,
+    image: "24.webp"
+  },
+  {
+    id: 25,
+    name: "Tênis Nike Journey Run Feminino",
+    price: 664.99,
+    image: "25.webp"
+  },
+  {
+    id: 26,
+    name: "TShorts Jordan Dri-FIT Sport Masculino",
+    price: 664.99,
+    image: "26.webp"
+  },
+  {
+    id: 27,
+    name: "Shorts Jordan Essentials Woven Masculino",
+    price: 284.99,
+    image: "27.webp"
+  },
+  {
+    id: 28,
+    name: "Shorts Nike Sportswear Essentials Woven Feminino",
+    price: 170.99,
+    image: "28.webp"
+  },
+  {
+    id: 29,
+    name: "Shorts Nike Sportswear Everything Wovens Feminino",
+    price: 199.99,
+    image: "29.webp"
+  },
+  {
+    id: 30,
+    name: "Camiseta Nike Pro Dri-FIT Masculina",
+    price: 180.49,
+    image: "30.webp"
+  },
+  {
+    id: 31,
+    name: "Camiseta Nike SB Masculina Preta Dri-Fit",
+    price: 84.00,
+    image: "31.webp"
+  },
+  {
+    id: 32,
+    name: "Camiseta Nike Sportswear Club Essentials Feminina",
+    price: 66.49,
+    image: "32.webp"
+  },
+  {
+    id: 33,
+    name: "Camiseta Nike Dri-FIT Feminina Rosa",
+    price: 94.99,
+    image: "33.webp"
+  },
+  {
+    id: 34,
+    name: "Chuteira Sem Cadarços Predator Elite Campo",
+    price: 1999.99,
+    image: "34.webp"
+  },
+  {
+    id: 35,
+    name: "Chuteira F50 Sparkfusion Club Campo/Grama Artificial",
+    price: 499.99,
+    image: "35.webp"
+  },
+  {
+    id: 36,
+    name: "Tênis de treino Amplimove Adidas Jaguar",
+    price: 499.99,
+    image: "36.webp"
+  },
+  {
+    id: 37,
+    name: "Tênis de Treino Dropset 3 Adidas Jaguar",
+    price: 899.99,
+    image: "37.webp"
+  },
+
+  {
+    id: 38,
+    name: "Camiseta Tennis Climacool+ AIRCHILL FreeLift",
+    price: 249.99,
+    image: "38.webp"
+  },
+
+  {
+    id: 40,
+    name: "Camiseta Treino Alongamento Train Essentials",
+    price: 179.99,
+    image: "40.webp"
+  },
+  {
+    id: 39,
+    name: "Camiseta Nike Sportswear Club Essentials Feminina",
+    price: 66.49,
+    image: "39.webp"
+  },
+  {
+    id: 41,
+    name: "Blusa Cropped Manga Longa Adicolor Futebol Jacquard",
+    price: 299.99,
+    image: "41.webp"
+  },
+  {
+    id: 42,
+    name: "Mochila Base Small Feminina",
+    price: 169.99,
+    image: "42.webp"
+  },
+  {
+    id: 43,
+    name: "Mochila Base Small Feminina",
+    price: 169.99,
+    image: "43.webp"
+  },
+  {
+    id: 44,
+    name: "Mochila PUMA S Heather",
+    price: 269.99,
+    image: "44.webp"
+  },
+  {
+    id: 45,
+    name: "Mochila PUMA PHASE Hooded",
+    price: 199.99,
+    image: "45.webp"
+  },
+   {
+    id: 46,
+    name: "Mini Bola PUMA Cumbre Oficial Conmebol Copa América",
+    price: 59.99,
+    image: "46.webp"
+  },
+   {
+    id: 47,
+    name: "Mini Bola Orbita LaLiga 1 MS Espanha",
+    price: 59.99,
+    image: "47.webp"
+  },
+   {
+    id: 48,
+    name: "Bola PUMA Cumbre Oficial Conmebol Copa America HYB",
+    price: 59.99,
+    image: "48.webp"
+  },
+  {
+    id: 49,
+    name: "Bola PUMA Cumbre Oficial Conmebol Copa America HYB",
+    price: 119.99,
+    image: "49.webp"
+  },
+  {
+    id: 50,
+    name: "Garrafa Training Sportstyle Puma Br",
+    price: 99.99,
+    image: "50.webp"
+  },
+  {
+    id: 51,
+    name: "Garrafa Training Sportstyle Puma Br",
+    price: 99.99,
+    image: "51.webp"
+  },
+  {
+    id: 52,
+    name: "Garrafa Shaker PUMA Preta 500ML",
+    price: 79.99,
+    image: "52.webp"
+  },
+  {
+    id: 53,
+    name: "Garrafa Training Sportstyle Litio 1L Puma",
+    price: 129.99,
+    image: "53.webp"
+  },
+  {
+    id: 54,
+    name: "Camisa Olympique de Marseille 25/26 Home Jersey Masculina",
+    price: 429.99,
+    image: "54.webp"
+  },
+  {
+    id: 55,
+    name: "Camisa Olympique de Marseille 24/25 AWAY Masculina",
+    price: 429.99,
+    image: "55.webp"
+  },
+  {
+    id: 56,
+    name: "Camisa Olympique de Marseille 24/25 AWAY Juvenil",
+    price: 359.99,
+    image: "56.webp"
+  },
+  {
+    id: 57,
+    name: "Camisa Olympique de Marseille 23/24 AWAY Juvenil",
+    price: 209.99,
+    image: "57.webp"
+  },
+  {
+    id: 58,
+    name: "Camisa Bahia Esporte Clube Torcedor Away 2025",
+    price: 249.99,
+    image: "58.webp"
+  },
+  {
+    id: 59,
+    name: "Camisa Bahia Esporte Clube Torcedor Home 2025",
+    price: 249.99,
+    image: "59.webp"
+  },
+  {
+    id: 60,
+    name: "Camisa Bahia Esporte Clube Superman Torcedor 2025",
+    price: 249.99,
+    image: "60.webp"
+  },
+  {
+    id: 61,
+    name: "Camiseta Casuals Bahia Esporte Clube 2025",
+    price: 249.99,
+    image: "61.webp"
+  },
 ];
